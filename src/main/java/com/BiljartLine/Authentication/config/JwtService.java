b7c9e1918d7e8c22e03013d9c8bd99f883b4bc69
Lars-v-Delft <lars171998@gmail.com>
2023-12-22 12:55:47 +0100

    fixed error handling

--- src/main/java/com/BiljartLine/Authentication/config/JwtService.java
+++ src/main/java/com/BiljartLine/Authentication/config/JwtService.java
@@ -1,23 +1,25 @@
 package com.BiljartLine.Authentication.config;
 
-import com.BiljartLine.Authentication.exceptions.InvalidArgumentException;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
-import org.springframework.security.authentication.BadCredentialsException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
 import javax.crypto.spec.SecretKeySpec;
 import java.security.Key;
 import java.util.Date;
+import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class JwtService {
+    private final UserDetailsService userDetailsService;
     private final Key key = new SecretKeySpec("PhnBSsuGwqJxsdqYZTBpt5pC7fQWjB3T".getBytes(), SignatureAlgorithm.HS256.getJcaName());
 
-    public String generateToken(UserDetails userDetails){
+    public String generateToken(UserDetails userDetails) {
         return Jwts
                 .builder()
                 .setSubject(userDetails.getUsername())
@@ -27,29 +29,21 @@ public class JwtService {
                 .compact();
     }
 
-    public void validate(String jwt){
-        try {
-            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwt);
-        } catch (Exception e){
-            throw new BadCredentialsException("Invalid token");
-        }
+    public void validate(String jwt) {
+        Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwt);
     }
 
-    public boolean isJWTExpired(String jwt){
-        return new Date().after(getExpiration(jwt));
-    }
-
-    public String getSubject(String jwt){
+    public String getSubject(String jwt) {
         Claims allClaims = extractAllClaims(jwt);
         return allClaims.getSubject();
     }
 
-    public Date getExpiration(String jwt){
+    public Date getExpiration(String jwt) {
         Claims allClaims = extractAllClaims(jwt);
         return allClaims.getExpiration();
     }
 
-    private Claims extractAllClaims(String jwt){
+    private Claims extractAllClaims(String jwt) {
         return Jwts
                 .parserBuilder()
                 .setSigningKey(key)
--- src/main/java/com/BiljartLine/Authentication/exceptions/ControllerExceptionHandler.java
+++ src/main/java/com/BiljartLine/Authentication/exceptions/ControllerExceptionHandler.java
@@ -1,5 +1,9 @@
 package com.BiljartLine.Authentication.exceptions;
 
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.security.SignatureException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.BadCredentialsException;
@@ -15,6 +19,13 @@ import java.util.Map;
 @Slf4j
 @RestControllerAdvice
 public class ControllerExceptionHandler {
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public ErrorMessage handleException(Exception e) {
+        log.warn("An Exception occurred", e);
+        return new ErrorMessage(HttpStatus.INTERNAL_SERVER_ERROR, "An error occurred");
+    }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
@@ -36,15 +47,50 @@ public class ControllerExceptionHandler {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(BadCredentialsException.class)
-    public ErrorMessage handleApiInvalidArgumentException(BadCredentialsException e) {
+    public ErrorMessage handleBadCredentialsException(BadCredentialsException e) {
         log.warn("An BadCredentialsException occurred", e);
-        return new ErrorMessage(HttpStatus.BAD_REQUEST, e.getMessage());
+        return new ErrorMessage(HttpStatus.BAD_REQUEST, "Credentials invalid");
     }
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(UsernameNotFoundException.class)
-    public ErrorMessage handleApiInvalidArgumentException(UsernameNotFoundException e) {
+    public ErrorMessage handleUsernameNotFoundException(UsernameNotFoundException e) {
         log.warn("An UsernameNotFoundException occurred", e);
-        return new ErrorMessage(HttpStatus.BAD_REQUEST, e.getMessage());
+        return new ErrorMessage(HttpStatus.BAD_REQUEST, "Username not found");
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(ExpiredJwtException.class)
+    public ErrorMessage handleExpiredJwtException(ExpiredJwtException e) {
+        log.warn("An ExpiredJwtException occurred", e);
+        return new ErrorMessage(HttpStatus.BAD_REQUEST, "JWT expired");
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(UnsupportedJwtException.class)
+    public ErrorMessage handleUnsupportedJwtException(UnsupportedJwtException e) {
+        log.warn("An UnsupportedJwtException occurred", e);
+        return new ErrorMessage(HttpStatus.BAD_REQUEST, "JWT not supported");
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MalformedJwtException.class)
+    public ErrorMessage handleMalformedJwtException(MalformedJwtException e) {
+        log.warn("An MalformedJwtException occurred", e);
+        return new ErrorMessage(HttpStatus.BAD_REQUEST, "JWT malformed");
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(SignatureException.class)
+    public ErrorMessage handleSignatureException(SignatureException e) {
+        log.warn("An SignatureException occurred", e);
+        return new ErrorMessage(HttpStatus.BAD_REQUEST, "JWT signature invalid");
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ErrorMessage handleIllegalArgumentException(IllegalArgumentException e) {
+        log.warn("An IllegalArgumentException occurred", e);
+        return new ErrorMessage(HttpStatus.BAD_REQUEST, "Bad argument");
     }
 }
