9b5f5e6e1569bb279175869cf51614a7f5a20736
Lars-v-Delft <lars171998@gmail.com>
2023-12-22 16:10:38 +0100

    Update AuthenticationApplicationTests.java

--- src/test/java/com/BiljartLine/Authentication/AuthenticationApplicationTests.java
+++ src/test/java/com/BiljartLine/Authentication/AuthenticationApplicationTests.java
@@ -3,11 +3,11 @@ package com.BiljartLine.Authentication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class AuthenticationApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class AuthenticationApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
